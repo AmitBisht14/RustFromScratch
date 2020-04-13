79ed769aa9c584dd048e03ac70fc6e83ab7d33ea
AmitBisht <Bishtamith@gmail.com>
2020-04-13 07:35:59 +0530

    array added

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,14 @@
 mod tuples;
 //to use specific pub function from above import -- @1
 use tuples::access_tuples;
+
+mod arrays;
+use arrays::access_array;
 fn main() {
     // if skip @1 then out of scope function can be consumed in below way
     // tuples::access_tuples();
+    println!("\r\ntuples:");
     access_tuples();
+    println!("\r\narrays:");
+    access_array();
 }
