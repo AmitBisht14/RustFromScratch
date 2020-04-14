6c6410dbe9ccafa5da40f2537731da7fbca87625
AmitBisht <Bishtamith@gmail.com>
2020-04-14 09:32:32 +0530

    folder structure created

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 //to import a different .rs file
-mod tuples;
+//it is important to specify each mod as pub else we won't be able to access it
+//nesting will be done as per the folder structure and similar was done while using it
+mod primitives {
+    pub mod arrays;
+    pub mod slices;
+    pub mod tuples;
+}
+mod custom_types {
+    pub mod structs;
+}
 //to use specific pub function from above import -- @1
-use tuples::access_tuples;
-
-mod arrays;
-mod slices;
-mod structs;
-use arrays::access_array;
-use slices::access_slice;
-use structs::access_structs;
+use custom_types::structs::access_structs;
+use primitives::arrays::access_array;
+use primitives::slices::access_slice;
+use primitives::tuples::access_tuples;
 
 fn main() {
     // if skip @1 then out of scope function can be consumed in below way
